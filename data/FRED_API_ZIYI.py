436c66055e612f65ee93c45546876ad573d75b71
ziyilu2019 <ZiyiLu2019@u.northwestern.edu>
2018-10-15 17:43:24 -0500

    Update FRED_API_ZIYI.py

--- data/FRED_API_ZIYI.py
+++ data/FRED_API_ZIYI.py
@@ -12,53 +12,8 @@ import pandas as pd
 import csv
 import unicodecsv
 import numpy as np 
-
-# Class Fred
-class Fred:
-    
-    def __init__(s):
-        s.rooturl  = 'https://api.stlouisfed.org/fred/series/observations?' 
-        s.api_key = '7531e59cd91f845d43fb1c7ac9535c57' 
-        s.file_type = 'json'
-        s.offset = 0
-        s.units = 'lin'
-        s.sord = 'asc'
-
-    # GET DATA
-    def getData(s, seriesid, start='', end='', limit=10000):
-        
-        # Packaging request
-        params = [
-            "api_key={0}".format(s.api_key),
-            "series_id={0}".format(seriesid),
-            "file_type={0}".format(s.file_type),
-            "sort_order={0}".format(s.sord),
-            "observation_start={0}".format(start),
-            "offset={0}".format(s.offset),
-            "limit={0}".format(limit),
-            "units={0}".format(s.units),  
-            "observation_end={0}".format(end)              
-        ]
-        rurl = s.rooturl + "&".join(params)            
-
-        # Making call
-        s.data = json.load(urllib.urlopen(rurl))
-        
-        return s.data        
-        
-    
-    def cleanData(s):
-        obs = s.data['observations']
-        ts_index = pd.to_datetime([d['date'] for d in obs], infer_datetime_format=True)
-        ts = pd.Series([d['value'] for d in obs], index=ts_index)
-        ts = pd.to_numeric(ts)
-        
-        # Rolling to monthly
-        ts_monthly = ts.resample('M').mean()
-        cleaned = ts_monthly.interpolate(method='spline', order=3) #using cubic spline method
-       
-        return cleaned
-
+import pandas_datareader.data as web
+import datetime
 
 def search(term):
         
@@ -74,31 +29,25 @@ def search(term):
      
     return pairs
 
+# Load in county names of MN
+path = "~/Desktop/MinnesodaCountryName&ID.csv"
+ct_names = pd.read_csv(path)
+start = datetime.datetime(1991, 1, 1)
+end = datetime.datetime(2018, 10, 15)
 
-d = search('minnesota')
-df = pd.DataFrame(d)
-df.columns = ['ID', 'Title']
-df = df.drop_duplicates(subset='Title')
-
-# clear out already written file
-file_path = '/Users/ziyilu/Desktop/data_list.csv'
-f = open(file_path, "w+")
-f.close()
-df.to_csv(file_path, encoding='utf-8') # list of tuple pairs of ID and Titles
-
-
-id_total_lst = []
-term_total_lst = []
-for x in d:
-    id_total_lst += df
+for i in range(ct_names.shape[0]):
+    data_total = pd.DataFrame()
     
+    county = ct_names.iloc[i,1]
+    search_term = county + ', MN'
+    d = search(search_term)
+    df = pd.DataFrame(d)
+    df.columns = ['ID', 'Title']
+    df = df.drop_duplicates(subset='Title')
+    id=df['ID']
 
-# create csv files for data
-f = Fred()
-directory = '/Users/ziyilu/Desktop/Data'
-for i in df['ID']:
-    id = i
-    rawdata = f.getData(id, start = '1991-01-01', end = '2018-10-15')
-    cleandata = f.cleanData()
-    path = '{0}/{1}.csv'.format(directory,id)
-    cleandata.to_csv(path, encoding='utf-8')
+    county_ts = web.DataReader(id, 'fred', start, end)
+    
+    # data cleaning
+    
+    county_ts.to_csv(county + '_Data.csv',encoding='utf-8')
