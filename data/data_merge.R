6e44e69f0f7a1f710dbdd0e4751ca53f71f316e6
joshwlivingston <JLivingston@u.northwestern.edu>
2018-10-28 15:54:47 -0500

    cleansing merged dataset

--- data/data_merge.R
+++ data/data_merge.R
@@ -9,7 +9,8 @@ county_acs <- read_csv("./data/processed/combined_counties/combined_counties.csv
 files <- dir("./data/unprocessed/Census Data/", ".csv$", full.names = TRUE)[1:8]
 
 comp_dat <- results %>% 
-  left_join(county_acs, by = c("countycode", "year"))
+  left_join(county_acs, by = c("countycode", "year")) %>% 
+  filter(!is.na(eq_sprime))
 
 write_rds(comp_dat, "./data/processed/competition_data.rds")
 
