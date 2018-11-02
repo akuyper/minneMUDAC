c7426b8c89656bc0027ed2bd06500971035c3167
joshwlivingston <JLivingston@u.northwestern.edu>
2018-11-01 21:31:38 -0500

    correcting issues in r_factor

--- data/factor_r_calculation.R
+++ data/factor_r_calculation.R
@@ -5,7 +5,8 @@ competition_data <- readRDS("data/processed/competition_data.rds")
 colnames(competition_data)
 
 competition_data <- 
-  competition_data %>% 
+  competition_data %>%
+  filter(reg7am != 0) %>% 
   mutate(r_factor=(totvoting-edr)/reg7am)
 
 write.csv(competition_data, file = "data/processed/competition_data_with_r.csv")
